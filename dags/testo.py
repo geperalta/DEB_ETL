43580ab28c399d25c864c8be0c1a2f40c3687d93
Gerardo Peralta <peraltagerar@gmail.com>
2022-07-16 01:17:46 -0500

    Fixed PostgresOperation sintax

--- dags/testo.py
+++ dags/testo.py
@@ -9,10 +9,10 @@ with DAG("testo_dago", start_date=days_ago(1), schedule_interval="@once"
     start_workflow = DummyOperator(task_id="start_workflow")
     validate = DummyOperator(task_id="validate")
     prepare = PostgresOperator(
-        task_id="prepare"
-        postgres_conn_id="ml_conn"
+        task_id="prepare",
+        postgres_conn_id="ml_conn",
         sql=""
-        )
+    )
     load = DummyOperator(task_id="load")
     end_workflow = DummyOperator(task_id="end_workflow")
 
