e9d3afbfe36250092c9f74323558aaa750586832
Gerardo Peralta <peraltagerar@gmail.com>
2022-07-15 23:41:49 -0500

    First test dag

---
+++ dags/testo.py
@@ -0,0 +1,12 @@
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+with DAG("testo_dago", start_date=days_ago(1)) as dag:
+    start_workflow = DummyOperator(task_id="start_workflow")
+    validate = DummyOperator(task_id="validate")
+    prepare = DummyOperator(task_id="prepare")
+    load = DummyOperator(task_id="load")
+    end_workflow = DummyOperator(task_id="start_workflow")
+
+    start_workflow >> validate >> prepare >> load >> end_workflow
