f95a446454fa16a9a3f4bb9a00002fa86b528049
Gerardo Peralta <peraltagerar@gmail.com>
2022-07-18 14:43:47 -0500

    Fixed typo for import again

--- dags/testo.py
+++ dags/testo.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
-from airflow.operators.sql import BranchSqlOperator
+from airflow.operators.sql import BranchSQLOperator
 
 def ingest_data():
     hook = PostgresHook(postgres_conn_id = "ml_conn")
@@ -51,8 +51,8 @@ with DAG("testo_dago", start_date=days_ago(1), schedule_interval="@once"
         sql="""DELETE FROM WIZESCHEMA.user_purchase""",
     )
     continue_workflow = DummyOperator(task_id="continue_workflow")
-    branch = BranchSqlOperator (
-        task_id='is_empty',
+    branch = BranchSQLOperator (
+        task_id='is_empty??',
         conn_id='ml_conn',
         sql="SELECT COUNT(*) AS rows FROM WIZESCHEMA.user_purchase", 
         #if the resould count is 1 the statement its TRUE and rune if_true branch
@@ -62,6 +62,6 @@ with DAG("testo_dago", start_date=days_ago(1), schedule_interval="@once"
     load = PythonOperator(task_id="load", python_callable=ingest_data)
     end_workflow = DummyOperator(task_id="end_workflow")
 
-    #DAGs order to execute
+    #DAGs order to execute. Downstring
     start_workflow >> validate >> prepare >> branch
     branch >> [clear, continue_workflow] >> load >> end_workflow
